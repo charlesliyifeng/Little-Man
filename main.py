a2944d59c239b3a75b6defdd32a7e747f21c8d3d
Charles <>
2021-05-20 05:10:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+TOKEN = os.environ['TOKEN']
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f"logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('$hello'):
+    await message.channel.send("Hello!")
+
+client.run(TOKEN)
