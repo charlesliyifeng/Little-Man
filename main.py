b51358f93fc484df36843e2f8e96a1c60d9b58fc
Charles <>
2021-05-21 09:21:15 +0000

    functions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import pandas
+from datetime import datetime
 import os
 
 TOKEN = os.environ['TOKEN']
@@ -8,12 +10,20 @@ client = discord.Client()
 async def on_ready():
   print(f"logged in as {client.user}")
 
+
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content.startswith('$hello'):
-    await message.channel.send("Hello!")
+  #if message.content.startswith('$'):
+  await message.channel.send("SHUT UP!")
+
+def check_time(message):
+  now = datetime.now()
+
+
+def check_swearing():
+  pass
 
 client.run(TOKEN)
