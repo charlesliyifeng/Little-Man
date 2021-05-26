af53e8b865d489981386fba76e41fd2abf677273
Charles <>
2021-05-26 09:16:13 +0000

    check time

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A discord bot responsible for managing Macleans Coding Club.
 ## todo list
 1. Command: /isJavascriptGood, Result: “no” is outputted in chat
 2. rules enforcement - up time,swearing,spamming
+3. Command: !codingHelp [message]
+4. !codingHelpResolve [name]
+5. !codingHelpRole [role]
+6. link google classroom updates
--- main.py
+++ main.py
@@ -10,20 +10,39 @@ client = discord.Client()
 async def on_ready():
   print(f"logged in as {client.user}")
 
-
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   
-  if message.content.startswith('$hello'):
+  if check_time():
+    warning(message,"chatting in class time")
+  
+  if message.content.startswith('!hello'):
     await message.channel.send("Hello!")
+  elif message.content.startswith('!isJavascriptGood'):
+    await message.channel.send("No")
 
-def check_time(message):
+#message restriction in schooltime
+def check_time():
+  today = datetime.today()
   now = datetime.now()
-
+  #12:30 - 13:15  13:00 - 13:35
+  if today.weekday() < 5:
+    if 8 < now.hour+now.minute/60 < 14.25:
+      if today.weekday() == 2: #wednesday
+        if 12 < now.hour+now.minute/60 < 12+7/12: #if in lunch time for wednesday:
+          return False
+      else:
+        if 12.5 < now.hour+now.minute/60 < 12.25:
+          return False
+      return True
+  return False
 
 def check_swearing():
   pass
 
+def warning(message,reason): #deletion of message and keeping trace of user
+  pass
+  
 client.run(TOKEN)
